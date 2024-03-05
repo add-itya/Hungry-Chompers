d43d7dbd037f3007f1080bfa60cea5b6e0a7915f
nowji <114260999+nowji@users.noreply.github.com>
2024-03-05 12:47:42 -0500

    Start of landing page

---
+++ website/src/components/home_page/home_page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home_page.css';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Hungry Chompers</h1>
+    </div>
+  );
+}
+
+export default HomePage;
