adcebe6a1c76f9d7deaa5b0316a08c00067bd2f9
nowji <114260999+nowji@users.noreply.github.com>
2024-03-06 15:18:27 -0500

    added log in features

--- website/src/components/home_page/home_page.js
+++ website/src/components/home_page/home_page.js
@@ -4,7 +4,24 @@ import './home_page.css';
 function HomePage() {
   return (
     <div className="container">
-      <h1>Hungry Chompers</h1>
+      <div className='WelcomeText'>
+        <h1>Welcome to Hungry Chompers</h1>
+        <p>Please sign in or create an account below.</p>
+      </div>
+      <div className='SignIn'>
+        <h2>Sign In</h2>
+        <form>
+          <label>
+            Email:
+            <input type="text" name="email" />
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <button className='SubmitButton'>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
