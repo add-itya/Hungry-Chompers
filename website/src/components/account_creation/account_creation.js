1bfd6f20617319f0f3efbc5bec0ff59ac0b8ae9f
Shawn Rhoads <76660850+shawn-rhoads@users.noreply.github.com>
2024-04-16 16:55:19 -0400

    added backend connection, account creation functionality
    backend connection and account creation with mongodb

--- website/src/components/account_creation/account_creation.js
+++ website/src/components/account_creation/account_creation.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import './account_creation.css';
+import { useState } from 'react'
+
 
 function AccountCreation() {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        let result = await fetch(
+        'http://localhost:5000/register', {
+            method: "post",
+            body: JSON.stringify({ email, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        result = await result.json();
+        console.warn(result);
+        if (result) {
+            alert("Data saved succesfully");
+            setpassword("");
+            setemail("");
+        }
+    }
     return (
-        <div className="container">
-            <div className='WelcomeText'>
+        <div classemail="container">
+            <div classemail='WelcomeText'>
                 <h1>Account Creation</h1>
-                <p>To make an account please enter an email and password below.</p>
+                <p>To make an account please enter an password and password below.</p>
             </div>
-            <div className='Creation'>
-                <form>
+            <div classemail='Creation'>
+                {/* <form>
                     <label>
-                        Email:
-                        <input type="text" name="email" />
+                        password:
+                        <input type="text" email="password" />
                     </label>
                     <label>
                         Password:
-                        <input type="password" name="password" />
+                        <input type="password" email="password" />
                     </label>
-                    <button className='SubmitButton'>Submit</button>
+                    <button classemail='SubmitButton'>Submit</button>
+                </form> */}
+                <form action="">
+                    <input type="text" placeholder="email"
+                    value={email} onChange={(e) => setemail(e.target.value)} />
+                    <input type="password" placeholder="password"
+                    value={password} onChange={(e) => setpassword(e.target.value)} />
+                    <button type="submit"
+                    onClick={handleOnSubmit}>submit</button>
                 </form>
             </div>
         </div>
